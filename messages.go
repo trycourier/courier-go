962e738cb1917fb0c79442e53d84d69a3f68aff4
Yousuf Ashparie <yousuf.ashparie@expel.io>
2020-05-22 11:09:49 -0400

    Update messages API response (#5)
    * Update the messages API for latest updates from Courier which includes a Providers response and Sent timestamps
    
    * Add message API fields

--- messages.go
+++ messages.go
@@ -7,16 +7,35 @@ import (
 	"net/http"
 )
 
+type ProvidersChannelResponse struct {
+	Key string
+	Name string
+	Template string
+}
+type ProvidersResponse struct {
+	Channel *ProvidersChannelResponse
+	Error string
+	Status string
+	Delivered int64
+	Sent int64
+	Clicked int64
+	Provider string
+	Reference interface{} // provider specific response
+}
+
 type MessageResponse struct {
 	Id string
+	Event string
+	Notification string
 	Status string
+	Error string
+	Reason string
+	Recipient string
 	Enqueued int64
 	Delivered int64
-	Provider string
-	RecipientId string
-	EventId string
-	Configuration string
-	ProviderResponse string
+	Sent int64
+	Clicked int64
+	Providers []*ProvidersResponse
 }
 
 func (c *Client) GetMessage(ctx context.Context, messageID string) (*MessageResponse, error) {
