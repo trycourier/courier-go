378493eea3b69f1e96f48638cc4624273bc237a3
Tejas Kumthekar <tejas.kumthekar26@gmail.com>
2021-01-28 19:22:59 -0600

    Send API

--- send.go
+++ send.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -12,18 +13,33 @@ type SendResponse struct {
 }
 
 type sendRequest struct {
-	EventID   string      `json:"event"`
-	Recipient string      `json:"recipient"`
-	Profile   interface{} `json:"profile"`
-	Data      interface{} `json:"data"`
+	EventID     string      `json:"event"`
+	Recipient   string      `json:"recipient"`
+	Profile     interface{} `json:"profile,omitempty"`
+	Data        interface{} `json:"data,omitempty"`
+	Brand       string      `json:"brand,omitempty"`
+	Override    interface{} `json:"override,omitempty"`
+	Preferences interface{} `json:"preferences,omitempty"`
 }
 
-func (c *Client) Send(ctx context.Context, eventID, recipientID string, profile, data interface{}) (string, error) {
+type sendToListRequest struct {
+	EventID  string      `json:"event"`
+	ListID   string      `json:"list,omitempty"`
+	Pattern  string      `json:"pattern,omitempty"`
+	Data     interface{} `json:"data,omitempty"`
+	Brand    string      `json:"brand,omitempty"`
+	Override interface{} `json:"override,omitempty"`
+}
+
+func (c *Client) Send(ctx context.Context, eventID string, recipientID string, profile interface{}, data interface{}, brand string, override interface{}, preferences interface{}) (string, error) {
 	payload := sendRequest{
-		EventID:   eventID,
-		Recipient: recipientID,
-		Profile:   profile,
-		Data:      data,
+		EventID:     eventID,
+		Recipient:   recipientID,
+		Profile:     profile,
+		Data:        data,
+		Brand:       brand,
+		Override:    override,
+		Preferences: preferences,
 	}
 
 	body, err := json.Marshal(payload)
@@ -32,6 +48,43 @@ func (c *Client) Send(ctx context.Context, eventID, recipientID string, profile,
 	}
 
 	req, err := http.NewRequestWithContext(ctx, "POST", c.BaseUrl+"/send", bytes.NewReader(body))
+	if err != nil {
+		return "", err
+	}
+
+	bytes, err := c.doRequest(req)
+	if err != nil {
+		return "", err
+	}
+	var responseData SendResponse
+	err = json.Unmarshal(bytes, &responseData)
+	if err != nil {
+		return "", err
+	}
+
+	return responseData.MessageId, nil
+}
+
+func (c *Client) SendToList(ctx context.Context, eventID string, listID string, pattern string, data interface{}, brand string, override interface{}) (string, error) {
+	if (listID == "" && pattern == "") || (listID != "" && pattern != "") {
+		return "", errors.New("list.send requires a listID or a pattern")
+	}
+
+	payload := sendToListRequest{
+		EventID:  eventID,
+		ListID:   listID,
+		Pattern:  pattern,
+		Data:     data,
+		Brand:    brand,
+		Override: override,
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", c.BaseUrl+"/send/list", bytes.NewReader(body))
 	if err != nil {
 		return "", err
 	}
--- send_test.go
+++ send_test.go
@@ -21,11 +21,20 @@ func TestSend(t *testing.T) {
 	type Profile struct {
 		Email string
 	}
+	type Override struct {
+		Bar string
+	}
+	type Preferences struct {
+		Fizz string
+	}
 	type RequestBody struct {
-		Event     string
-		Recipient string
-		Profile   Profile
-		Data      Data
+		Event       string
+		Recipient   string
+		Profile     Profile
+		Data        Data
+		Brand       string
+		Override    Override
+		Preferences Preferences
 	}
 
 	var requestBody RequestBody
@@ -64,7 +73,14 @@ func TestSend(t *testing.T) {
 		}
 		eventID := "event-id"
 		recipientID := "recpient-id"
-		messageID, err := client.Send(context.Background(), eventID, recipientID, profile, data)
+		brand := "brand-id"
+		override := &Override{
+			Bar: "bar",
+		}
+		preferences := &Preferences{
+			Fizz: "fizz",
+		}
+		messageID, err := client.Send(context.Background(), eventID, recipientID, profile, data, brand, override, preferences)
 
 		assert.Nil(t, err)
 		assert.Equal(t, expectedResponseID, messageID)
@@ -72,6 +88,78 @@ func TestSend(t *testing.T) {
 		assert.Equal(t, recipientID, requestBody.Recipient)
 		assert.Equal(t, data.Foo, requestBody.Data.Foo)
 		assert.Equal(t, profile.Email, requestBody.Profile.Email)
+		assert.Equal(t, brand, requestBody.Brand)
+		assert.Equal(t, override.Bar, requestBody.Override.Bar)
+		assert.Equal(t, preferences.Fizz, requestBody.Preferences.Fizz)
+	})
+
+}
+
+func TestSendToList(t *testing.T) {
+
+	type Data struct {
+		Foo string
+	}
+
+	type Override struct {
+		Bar string
+	}
+
+	type RequestBody struct {
+		Event    string
+		List     string
+		Pattern  string
+		Data     Data
+		Brand    string
+		Override Override
+	}
+
+	var requestBody RequestBody
+
+	expectedResponseID := "123456789"
+	server := httptest.NewServer(http.HandlerFunc(
+
+		func(rw http.ResponseWriter, req *http.Request) {
+
+			assert.Equal(t, "/send/list", req.URL.String())
+
+			decoder := json.NewDecoder(req.Body)
+			err := decoder.Decode(&requestBody)
+			if err != nil {
+				t.Error(err)
+			}
+
+			rw.Header().Add("Content-Type", "application/json")
+			_, writeErr := rw.Write([]byte(fmt.Sprintf("{ \"MessageId\" : \"%s\" }", expectedResponseID)))
+			if writeErr != nil {
+				t.Error(writeErr)
+			}
+
+		}))
+	defer server.Close()
+
+	t.Run("sends request", func(t *testing.T) {
+
+		client := courier.CourierClient("key", server.URL)
+
+		data := &Data{
+			Foo: "bar",
+		}
+		eventID := "event-id"
+		listID := "list-id"
+		brand := "brand-id"
+		override := &Override{
+			Bar: "bar",
+		}
+		messageID, err := client.SendToList(context.Background(), eventID, listID, "", data, brand, override)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedResponseID, messageID)
+		assert.Equal(t, eventID, requestBody.Event)
+		assert.Equal(t, listID, requestBody.List)
+		assert.Equal(t, data.Foo, requestBody.Data.Foo)
+		assert.Equal(t, brand, requestBody.Brand)
+		assert.Equal(t, override.Bar, requestBody.Override.Bar)
 	})
 
 }
