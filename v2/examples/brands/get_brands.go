953fc02bbef6928b1a0c1a6edecf5ce9d7a024ab
tejas <6154318+tk26@users.noreply.github.com>
2021-05-07 16:18:23 -0700

    Add Brands API (#25)

---
+++ v2/examples/brands/get_brands.go
@@ -0,0 +1,21 @@
+package examples
+
+import (
+	"context"
+	"log"
+
+	"github.com/trycourier/courier-go/v2"
+)
+
+func getBrands() {
+	client := courier.CreateClient("<YOUR_AUTH_TOKEN>", nil)
+
+	data, err := client.GetBrands(context.Background(),
+		"", // optional cursor
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(data)
+}
