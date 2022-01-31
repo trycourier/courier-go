3d56cc40bf76133b6692ce7b45bd6c01d769093a
tejas <6154318+tk26@users.noreply.github.com>
2022-01-31 10:01:34 -0800

    C-4854: Support new features in Send API (#31)

--- v2/send.go
+++ v2/send.go
@@ -13,6 +13,12 @@ type SendBody struct {
 	Brand    string      `json:"brand,omitempty"`
 }
 
+// SendMessageRequestBody is the JSON body expected by
+// Courier's /send endpoint that accepts a message object
+type SendMessageRequestBody struct {
+	Message interface{} `json:"message"`
+}
+
 // Send calls the /send endpoint of the Courier API (passing a struct)
 func (c *Client) Send(ctx context.Context, eventID, recipientID string, body interface{}) (string, error) {
 	bodyMap, err := toJSONMap(body)
@@ -39,4 +45,28 @@ func (c *Client) SendMap(ctx context.Context, eventID, recipientID string, body 
 		return "", err
 	}
 	return messageID, nil
+}
+
+// SendMessage calls the enhanced /send endpoint of the Courier API with a message struct
+func (c *Client) SendMessage(ctx context.Context, body SendMessageRequestBody) (string, error) {
+	bodyMap, err := toJSONMap(body)
+	if err != nil {
+		return "", err
+	}
+	return c.SendMessageMap(ctx, bodyMap)
+}
+
+// SendMessageMap calls the enhanced /send endpoint of the Courier API with maps
+func (c *Client) SendMessageMap(ctx context.Context, body map[string]interface{}) (string, error) {
+	response, err := c.API.SendRequestWithMaps(ctx, "POST", "/send", body)
+	if err != nil {
+		return "", err
+	}
+
+	var requestID string
+	err = json.Unmarshal(response["requestId"], &requestID)
+	if err != nil {
+		return "", err
+	}
+	return requestID, nil
 }
--- v2/send_test.go
+++ v2/send_test.go
@@ -301,3 +301,46 @@ func TestSend_Send_400Error(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, err.(*courier.HTTPError).StatusCode)
 	})
 }
+
+func TestSend_SendMessage(t *testing.T) {
+	var requestBody courier.SendMessageRequestBody
+	url := "/send"
+	requestID := "123456789"
+
+	server := httptest.NewServer(http.HandlerFunc(
+		func(rw http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, url, req.URL.String())
+
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(req.Body)
+
+			err := json.Unmarshal(buf.Bytes(), &requestBody)
+			if err != nil {
+				t.Error(err)
+			}
+
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Add("Content-Type", "application/json")
+			_, writeErr := rw.Write([]byte(fmt.Sprintf("{ \"requestId\" : \"%s\" }", requestID)))
+			if writeErr != nil {
+				t.Error(writeErr)
+			}
+		}),
+	)
+	defer server.Close()
+
+	t.Run("sends request", func(t *testing.T) {
+		client := courier.CreateClient("key", &server.URL)
+		result, err := client.SendMessage(context.Background(), courier.SendMessageRequestBody{
+			Message: map[string]interface{}{
+				"template": "my-template",
+				"to": map[string]string{
+					"email": "foo@bar.com",
+				},
+			},
+		})
+
+		assert.Nil(t, err)
+		assert.Equal(t, requestID, result)
+	})
+}
