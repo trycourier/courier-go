027c71ccbda96f18b1b18057a0cda236ddc1c283
Alexander Guzman <alexander@courier.com>
2022-03-23 15:05:56 -0400

    [C-5501] Alexander/c 5501 Test send_message with timeout prop (#34)
    * Adding test and docs for timeouts
    
    * Missing trailing comma
    
    * Fixing name

---
+++ v2/examples/send/send_message_timeout.go
@@ -0,0 +1,27 @@
+package examples
+
+import (
+	"context"
+	"log"
+
+	"github.com/trycourier/courier-go/v2"
+)
+
+func sendMessageTimeout() {
+	client := courier.CreateClient("<YOUR_AUTH_TOKEN>", nil)
+
+	requestID, err := client.SendMessage(context.Background(), courier.SendMessageRequestBody{
+		Message: map[string]interface{}{
+			"template": "my-template",
+			"to": map[string]string{
+				"email": "foo@bar.com",
+			},
+			"timeout": int64(3600000), // 1 hour in milliseconds
+		},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(requestID)
+}
