505c16513db16bf0fc4851768517ff4b28c31723
Ben Alderfer <ben.alderfer@expel.io>
2022-08-12 16:35:48 -0400

    Remove data pointer

--- v2/webhooks.go
+++ v2/webhooks.go
@@ -2,6 +2,6 @@ package courier
 
 // WebhookResponse represents the payload provided by an outbound webhook
 type WebhookResponse struct {
-	Data *MessageResponse `json:"data"`
-	Type string           `json:"type"`
+	Data MessageResponse `json:"data"`
+	Type string          `json:"type"`
 }
