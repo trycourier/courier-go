a91645e7566e84830945c774730f7ed69b7c8d2c
Ben Alderfer <BenAlderfer@users.noreply.github.com>
2023-11-03 13:09:10 -0400

    Add cancel message endpoint (#60)
    This adds support for the cancel message endpoint - https://www.courier.com/docs/reference/logs/cancel/
    I also added in some missing fields to the MessageResponse struct and reworked the tests to better handle testing multiple endpoints

--- v2/messages.go
+++ v2/messages.go
@@ -2,6 +2,7 @@ package courier
 
 import (
 	"context"
+	"fmt"
 )
 
 // ProvidersChannelResponse represents the channel section of the ProvidersResponse
@@ -26,24 +27,40 @@ type ProvidersResponse struct {
 
 // MessageResponse represents the return of the /messages/* endpoints on the Courier API
 type MessageResponse struct {
-	ID           string
-	Event        string
-	Notification string
-	Status       string
-	Error        string
-	Reason       string
-	Recipient    string
-	Enqueued     int64
-	Delivered    int64
-	Sent         int64
-	Clicked      int64
-	Providers    []*ProvidersResponse
+	ID            string
+	Event         string
+	Notification  string
+	Status        string
+	Error         string
+	Reason        string
+	Recipient     string
+	JobId         string
+	ListId        string
+	ListMessageId string
+	RunId         string
+	Enqueued      int64
+	Delivered     int64
+	Sent          int64
+	Clicked       int64
+	Opened        int64
+	CanceledAt    int64
+	Providers     []*ProvidersResponse
 }
 
 // GetMessage calls the /messages/:id endpoint of the Courier API
 func (c *Client) GetMessage(ctx context.Context, messageID string) (*MessageResponse, error) {
 	var response MessageResponse
 	err := c.API.SendRequestWithJSON(ctx, "GET", "/messages/"+messageID, nil, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+// CancelMessage calls the /messages/:id/cancel endpoint of the Courier API
+func (c *Client) CancelMessage(ctx context.Context, messageID string) (*MessageResponse, error) {
+	var response MessageResponse
+	err := c.API.SendRequestWithJSON(ctx, "POST", fmt.Sprintf("/messages/%s/cancel", messageID), nil, &response)
 	if err != nil {
 		return nil, err
 	}
